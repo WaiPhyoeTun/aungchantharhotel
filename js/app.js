4af373f5ff39bf69c29b04864fe914563d3e4a5d
WaiPhyoeTun <100112205+WaiPhyoeTun@users.noreply.github.com>
2022-12-08 11:19:43 +0630

    8 Dec 2022

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function dropbtn(e){
     //   console.log('hi');
 
     // underfined
+
     // console.log(e.target);
     // console.log(e.target.nextElementSibling);
 
@@ -92,75 +93,177 @@ setInterval(autoload,2500);
 // End Auto Background
 
 
-// Start Testimonial Seciton 
-const getcompanyname = document.querySelector('.companyname');
-const getrole = document.querySelector('.role');
-const gettestimonial = document.querySelector('.testimonial');
+// End Header
 
+// Start Testimonials
+    const getcpmpanyname = document.querySelector('.companyname')
+    const getrole = document.querySelector('.role')
+    const gettestimonial = document.querySelector('.testimonial')
 
-const testimonialdatas = [
-    {
-        name:'Rich Star Restaurant',
-        position:'Our Partner',
-        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-        name:'48 Sky Bar Cafe',
-        position:'Our Partner',
-        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-        name:'52 Beach Hotel',
-        position:'Our Partner',
-        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-        name:'Cool And Swimming Pool',
-        position:'Our Partner',
-        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-        name:'Chaung Thar SPA',
-        position:'Our Partner',
-        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    }
-
-];
+    const testimonialdatas=[
+        {
+            name:"Rich Star Restaurant",
+            position:"Our Outlet",
+            text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            name:"48 Sky Bar Cafe",
+            position:"Our Partner",
+            text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            name: "52 Beach Hotel" ,
+            position: "Our Branch" ,
+            text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            name: "Cool Land Swimming Pool" ,
+            position: "Our Client" ,
+            text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            name:"Chaung Thar SPA",
+            position: "Our Business",
+            text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+    ];
 
 let idx = 0;
+
 // console.log(testimonialdatas[idx]);
 // console.log(testimonialdatas[idx].name);
 // console.log(testimonialdatas[idx].position);
 // console.log(testimonialdatas[idx].text);
 
 function updatetestimonial(){
-    // getcompanyname.textContent = testimonialdatas[idx].name;
-    // getrole.text = testimonialdatas[idx].position;
+
+    // getcpmpanyname.textContent = testimonialdatas[idx].name;
+    // getrole.textContent = testimonialdatas[idx].position;
     // gettestimonial.textContent = testimonialdatas[idx].text;
 
-    const {name,position,text} = testimonialdatas[idx];
+
+    var {name,position,text} = testimonialdatas[idx];
+
     // console.log(name);
     // console.log(position);
     // console.log(text);
-
-    getcompanyname.textContent = name;
+    
+    getcpmpanyname.textContent = name;
     getrole.textContent = position;
     gettestimonial.textContent = text;
 
-    idx++;
 
-    if(idx > testimonialdatas.length -1){
-        idx = 0;
+    idx ++;
+
+    if(idx > testimonialdatas.length - 1){
+        idx = 0 ;
     }
 }
+
 updatetestimonial();
+
 setInterval(updatetestimonial,10000);
+    
+
+// End Testimonials
+// Start Properties Section
+
+filterby('all');
+
+function filterby(keyword){
+    // console.log(keyword);
+
+    var getfilters = document.getElementsByClassName('filters');
+
+    // console.log(getfilters);
+
+    if(keyword === 'all'){
+        keyword = ''
+    }
+
+    for(var x = 0 ; x < getfilters.length;x++){
+        // console.log(getfilters[x]);
+
+        // console.log(getfilters[x].className.indexOf(keyword) > -1);
+
+        removeshowclass(getfilters[x],'show');
+
+        if(getfilters[x].className.indexOf(keyword) > -1){
+            addshowclass(getfilters[x],'show');
+        }
+        
+    }
+}
+
+function addshowclass(ele,opt){
+    // console.log(ele);
+    var getfilters = ele.className.split(" ");
+    // console.log(getfilters);
+
+    var getopt = opt.split(" ");
+    // console.log(getopt);
+    // console.log(getopt.length);
+    // console.log(opt.length);
+
+    for(var y = 0 ; y < getopt.length ; y++){
+        // console.log(getopt[y]);
+
+        // console.log(getfilters.indexOf(getopt[y]) === -1 );
+        if(getfilters.indexOf (getopt[y]) === -1){
+            // ele.classList.addshowclass(getopt[y]);
+            
+            ele.className += " "+getopt[y];
+        }
+    }
+}
+
+function removeshowclass(ele,opt){
+    // console.log(ele);
+    var getfilters = ele.className.split(' ');
+    // console.log(getfilters);
+
+    var getopt = opt.split(' ');
+    // console.log(getopt);
+    // console.log(getopt.length);
+
+    for(var k = 0 ; k < getfilters.length ; k++){
+        
+        // console.log(getfilters[k]);
+        // console.log(getfilters[0]);
+        // console.log(getfilters[1]);
+
+        // console.log(getfilters[k]);
+
+        while(getfilters.indexOf(getopt[k]) > -1 ){
+            getfilters.splice(getfilters.indexOf(getopt[k]),1);
+        }
+    }
+
+    // console.log(getfilters);
+    // console.log(getfilters.join(' '));
+
+    ele.className = getfilters.join(' ');
+    // console.log(ele.className);
+
+}
+
+var getftcontrol = document.getElementById('filtercontrol');
+var getlis = getftcontrol.getElementsByClassName('list-inline-item');
+
+for(var i = 0 ; i < getlis.length;i++){
+    getlis[i].addEventListener('click',function(){
+        var curractives = document.querySelector('.activeitems');
+        curractives.className = curractives.className.replace('activeitems','');
+
+        this.className += ' activeitems';
+        // console.log(curractives);
+    });
+}
+
+// End Properties Section
+
+// 3TM
+
+// 1RM
 
 
-
-
-
-
-// End Testimonial Seciton 
-
-// End Header
